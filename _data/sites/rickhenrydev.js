142158522684745718125420bc13ce1def6a6c6e
Rick Henry <rickhenry@rickhenry.dev>
2022-06-08 13:53:29 -0400

    change to my sites
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- _data/sites/rickhenrydev.js
+++ _data/sites/rickhenrydev.js
@@ -1,26 +1,23 @@
 module.exports = {
-	description: "Front-end Testing Tools",
+	name: "rickhenrydev.netlify.app", // optional, falls back to object key
+	description: "Rick Henry Development site",
 	// skip if localhost
 	// skip if this is a new fork of the speedlify (not Zach’s)
 	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
 	options: {
-		frequency: 60 * 11 + 30, // 11h, 30m
+		frequency: 60 * 23, // 23 hours
 		// Use "run" if the sites don’t share assets on the same origin
 		//           and we can reset chrome with each run instead of
 		//           each site in every run (it’s faster)
 		// Use "site" if sites are all on the same origin and share assets.
-		freshChrome: "site",
+		freshChrome: "site"
 	},
 	urls: [
-		"https://eslint.org/",
-		"https://qunitjs.com/",
-		"https://karma-runner.github.io/latest/index.html",
-		"https://gulpjs.com/",
-		"https://webhint.io/",
-		"https://gruntjs.com/",
-		"https://theintern.io/",
-		"https://istanbul.js.org/",
-		"https://webdriver.io/",
-		"https://mochajs.org/"
+		"https://rickhenrydev.netlify.app/",
+		"https://rickhenrydev.netlify.app/plans/",
+		"https://rickhenrydev.netlify.app/projects/",
+		"https://rickhenrydev.netlify.app/about/",
+		"https://rickhenrydev.netlify.app/blog/",
+		"https://rickhenrydev.netlify.app/get-started/",
 	]
 };
